816633da39d72df7699bdb1e703d5721fded3ae4
Devon Deason <devon.d.deason@gmail.com>
2020-03-02 09:13:52 -0700

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  state = {
+    loading: true,
+    data: null,
+    error: null
+  };
+
+  componentDidMount() {}
+
+  render() {
+    if (this.state.loading) {
+      return <div className="App">Loading</div>;
+    }
+
+    if (this.state.data) {
+      return <div className="App">App Data</div>;
+    }
+  }
 }
 
 export default App;
