e34eed91d579d73aab5422def18365312e7d46e9
Devon Deason <devon.d.deason@gmail.com>
2020-03-04 07:57:16 -0700

    fix parsing on location text

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
     const { longitude, latitude } = position.coords;
     const [cityName, weatherData] = await Promise.all([
       axios.get(
-        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&result_type=locality&key=AIzaSyCSRfDuPVjVUmKFmIEFs4oLJwwngrVylrk`
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyCSRfDuPVjVUmKFmIEFs4oLJwwngrVylrk`
       ),
 
       // TODO - this cors-anywhere.herokuapp.com seems hacky, need to find a more proper solution
@@ -39,11 +39,31 @@ class App extends Component {
         `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/b2522f8b48049f38fd4bf5045e5c908f/${latitude},${longitude}`
       )
     ]);
+    let city, state;
+    const results = cityName.data.results;
 
-    console.log(cityName.data);
+    // The data returned from Google isn't formatted how we want it, we need to find just the types that match 'locality' {city} and 'administrative_area_level_1' {state} so we get something like Denver, CO
+    // TODO - This seems to only work for certain locations - need a better solution
+
+    for (let i = 0; i < results[0].address_components.length; i++) {
+      // prettier-ignore
+      for (let x = 0; x < results[0].address_components[i].types.length; x++) {
+          const type = results[0].address_components[i].types[x];
+          if (type === 'locality') { // Locality holds the city name
+              city = results[0].address_components[i].long_name;
+              break;
+          }
+
+          if (type === 'administrative_area_level_1') { // administrative_area_level_1 holds the state
+              state = results[0].address_components[i].long_name;
+              break;
+          }
+      }
+    }
+
     this.setState({
       loading: false,
-      locationText: cityName.data.results[0].formatted_address,
+      locationText: city + ", " + state,
       weatherData: weatherData.data
     });
   };
